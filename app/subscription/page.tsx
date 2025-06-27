b17159de9a47f2e5da7bf68126e0c5cff15a7687
chris-robert-yeslin2006 <robertchemist2006@gmail.com>
2025-06-27 23:44:35 +0530

    clerk billing

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,9 +1,12 @@
+import { PricingTable } from '@clerk/nextjs'
 import React from 'react'
 
 const Subscription = () => {
   return (
-    <div>Subscription</div>
+    <div>
+      <PricingTable />
+    </div>
   )
 }
-
+//looks like small but in clerk i done many things like added plans and add new features for each plans and then i added a subscription page
 export default Subscription
