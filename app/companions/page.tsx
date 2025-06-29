c4e99c7f1d9edf1599a7854e4201a0a875d8a181
chris-robert-yeslin2006 <robertchemist2006@gmail.com>
2025-06-29 11:13:15 +0530

    implemented companian library

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,40 @@
+import CompanionCard from '@/components/CompanianCard';
+import SearchInput from '@/components/SearchInput';
+import SubjectFilter from '@/components/SubjectFilter';
+import { getAllCompanions } from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
+import { stdout } from 'process';
 import React from 'react'
 
-const CompaniansLibrary = () => {
+const CompaniansLibrary = async({searchParams}:SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+  stdout.write(`Fetching companions for subject: ${subject} and topic: ${topic}\n`);
+
+  const companions=await getAllCompanions({subject, topic});
+  stdout.write(`Fetched ${companions.length} companions\n`);
+  stdout.write(`Companions: ${JSON.stringify(companions, null, 2)}\n`);
   return (
-    <div>CompaniansLibrary</div>
+    <main>
+            <section className="flex justify-between gap-4 max-sm:flex-col">
+                <h1>Companion Library</h1>
+                <div className="flex gap-4">
+                    <SearchInput />
+                    <SubjectFilter />
+                    
+                </div>
+            </section>
+            <section className="companions-grid">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
+        </main>
   )
 }
 
